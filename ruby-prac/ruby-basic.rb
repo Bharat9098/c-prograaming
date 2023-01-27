3122225a0a5ba85e601bb53ee64e585c702228c4
Bharat9098 <kha.bharat9098@gmail.com>
2023-01-27 18:51:40 +0530

    ruby basics

---
+++ ruby-prac/ruby-basic.rb
@@ -0,0 +1,42 @@
+# always new line
+
+puts "hello world"
+puts "hello world1"
+
+
+# prints on same line
+print "bharat"
+print "khatoliya"
+
+puts ""
+
+# +, -, *, /, %
+
+puts 1 + 2
+puts 1 - 2
+puts 1 * 2
+puts 1 / 2
+puts 1 % 2
+puts 5 ** 3
+i = 30
+puts i += 15
+puts 4        +                5
+
+
+time_now = Time.now
+
+puts "Hello World the time is #{time_now}"
+puts 'Hello World the time is #{time_now}'
+
+# string functions
+
+puts "my name is bharat".length
+puts "my name is bharat".reverse
+puts "my name is bharat".capitalize 
+puts "my name is bharat".upcase
+puts "MY NAME IS BHARAT".downcase
+puts "my name is bharat".next
+puts "my name is bharat".empty?
+puts "".empty?
+
+
